21109ff868a7210a876188d5325e17e722ff5cf8
Devchawla2608 <dev.chawla2608@gmail.com>
2022-11-04 19:37:59 +0530

    Update Client.js

--- frontend/assets/js/chatEngine/Client.js
+++ frontend/assets/js/chatEngine/Client.js
@@ -3,7 +3,7 @@ const socket = io("http://localhost:8000");
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageinp");
 const messageContainer = document.querySelector(".container");
-var audio = new Audio("./ting.mp3");
+var audio = new Audio("../../audio/ting.mp3");
 
 const append = (message, position) => {
   const messageElement = document.createElement("div");
