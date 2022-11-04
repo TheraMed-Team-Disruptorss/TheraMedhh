d2ffa1c18de4437b5fc2da6b9021bf7617783d56
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2022-11-05 04:46:20 +0530

    dgrt

--- mvc-main/index.js
+++ mvc-main/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const http= require("http");
+const socketio=require("socket.io");
+const server=http.createServer(app);
+const io=socketio(server)
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -37,4 +41,77 @@ app.use("/", require("./routes/PatientRoutes"));
 
 const PORT = process.env.PORT || 4111;
 
-app.listen(PORT, console.log("Server has started at port " + PORT));
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("New user", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+});
+})
+const { v4: uuidV4 } = require('uuid')
+
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+
+app.get('/video', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/video:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+server.listen(PORT, console.log("Server has started at port " + PORT));
+
+
+
+
+// const io = require("socket.io")(8000, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+
+// const users = {};
+
+// io.on("connection", (socket) => {
+//   socket.on("new-user-joined", (name) => {
+//     console.log("New user", name);
+//     users[socket.id] = name;
+//     socket.broadcast.emit("user-joined", name);
+//   });
+
+//   socket.on("send", (message) => {
+//     socket.broadcast.emit("receive", {
+//       message: message,
+//       name: users[socket.id],
+//     });
+//   });
+
+//   socket.on("disconnect", (message) => {
+//     socket.broadcast.emit("left", users[socket.id]);
+//     delete users[socket.id];
+//   });
+// });
