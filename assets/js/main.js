abcdaffe61661218c73db6b439b9da5ac3a42f08
Vani-Sharma <vanisharma826@gmail.com>
2022-11-05 10:38:38 +0530

    Mood tracking js updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -283,4 +283,40 @@ document.addEventListener('DOMContentLoaded', () => {
     aos_init();
   });
 
+
+
+
+  const btn = document.getElementById("btClickMe");
+  let get = getCookie("clicked"); // check cookie after page loaded
+  if (get) {
+    btn.disabled = true; // disable the button after page loaded
+  }
+
+  btn.addEventListener("click", function (_event) {
+    get = getCookie("clicked"); // check cookie exist
+    if (!get) setCookie("clicked", "true", 1); // set cookie after clicked
+    btn.disabled = true; // disable the button after button clicked
+
+  });
+
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
 });
